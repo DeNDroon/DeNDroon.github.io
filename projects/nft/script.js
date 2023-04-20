b346239f35f7359cc18555a732650293a41170bd
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-04-20 14:19:14 +0400

    fix

--- projects/nft/script.js
+++ projects/nft/script.js
@@ -1,11 +1,19 @@
-$("body").mousemove(function(e) {
-    posY = e.clientY / this.clientHeight * 45 - 22.5;
-    posX = e.clientX / this.clientWidth * 45 - 22.5;
-    $("#card").css("--x", -posY + "deg");
-    $("#card").css("--y", posX + "deg");
-    reflectPosY = e.clientY / this.clientHeight * 80;
-    reflectPosX = e.clientX / this.clientWidth * 70 - 35 + reflectPosY - 40;
+if (isMobileDevice()) {
+} else {
+$("body").mousemove(function (e) {
+
+        posY = e.clientY / this.clientHeight * 45 - 22.5;
+        posX = e.clientX / this.clientWidth * 45 - 22.5;
+        $("#card").css("--x", -posY + "deg");
+        $("#card").css("--y", posX + "deg");
+        reflectPosY = e.clientY / this.clientHeight * 80;
+        reflectPosX = e.clientX / this.clientWidth * 70 - 35 + reflectPosY - 40;
         // $("#reflect").css("top", reflectPosY + "%");
         // $("#reflect").css("left", reflectPosX + "%");
-    $("#reflect").css("--grad", reflectPosX + "deg");
+        $("#reflect").css("--grad", reflectPosX + "deg");
 })
+}
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}

816fc7252d032a073990d886e6be445bd0e58b19
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-04-20 16:19:39 +0400

    Upload

--- projects/nft/script.js
+++ projects/nft/script.js
@@ -1,4 +1,5 @@
 if (isMobileDevice()) {
+    $("#reflect").css("display","none");
 } else {
 $("body").mousemove(function (e) {
 
@@ -6,10 +7,7 @@ $("body").mousemove(function (e) {
         posX = e.clientX / this.clientWidth * 45 - 22.5;
         $("#card").css("--x", -posY + "deg");
         $("#card").css("--y", posX + "deg");
-        reflectPosY = e.clientY / this.clientHeight * 80;
         reflectPosX = e.clientX / this.clientWidth * 70 - 35 + reflectPosY - 40;
-        // $("#reflect").css("top", reflectPosY + "%");
-        // $("#reflect").css("left", reflectPosX + "%");
         $("#reflect").css("--grad", reflectPosX + "deg");
 })
 }

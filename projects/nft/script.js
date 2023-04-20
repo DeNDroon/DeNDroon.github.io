5e8bc38d13f8a185aa593b3324ff0aa9d025c362
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-04-20 14:06:16 +0400

    NFT upload

---
+++ projects/nft/script.js
@@ -0,0 +1,11 @@
+$("body").mousemove(function(e) {
+    posY = e.clientY / this.clientHeight * 45 - 22.5;
+    posX = e.clientX / this.clientWidth * 45 - 22.5;
+    $("#card").css("--x", -posY + "deg");
+    $("#card").css("--y", posX + "deg");
+    reflectPosY = e.clientY / this.clientHeight * 80;
+    reflectPosX = e.clientX / this.clientWidth * 70 - 35 + reflectPosY - 40;
+        // $("#reflect").css("top", reflectPosY + "%");
+        // $("#reflect").css("left", reflectPosX + "%");
+    $("#reflect").css("--grad", reflectPosX + "deg");
+})

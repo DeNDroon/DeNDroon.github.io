bd9e3ab35e9545474f8751702066afa318b19f1b
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-02 15:37:46 +0400

    Let's start journey

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-var timer = 0;
-var i = setInterval(function() {
-    timer+=1;
-    $("#load-scrn").css("stroke-dashoffset", timer + 'px');
-}, 10)
-
-$("#body").ready(function () {
-    clearInterval(i);
-    $("#load-screen").fadeOut(800);
-});
-
-document.body.addEventListener('mousemove', function(e) {
-    var scrnX = document.body.clientWidth;
-    var scrnY = document.body.clientHeight;
-    var posX = e.pageX;
-    var posY = e.pageY;
-    var grad_deg = (posX + scrnX) / (posY + scrnY) * 180;
-    document.body.style.setProperty("--grad-deg", grad_deg  + "deg");
-})
+let scrolling = false;
 
 document.addEventListener("scroll", function() {
-    var curPos = document.documentElement.scrollTop;
-    if(curPos>= 400) {
-        $("#nav-bot").css("top", curPos + "px");
+    if(document.documentElement.scrollTop >= 100) {
+        $("header > h1").css("opacity", "0");
     } else {
-        $("#nav-bot").css("top", 400 + "px");
+        $("header > h1").css("opacity", "1");
     }
 })

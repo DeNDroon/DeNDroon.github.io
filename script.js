5d057b4addade799120a02ee0c4fb3122ec9f36f
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-10 13:15:16 +0400

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", function() {
-    if(document.documentElement.scrollTop >= 100) {
-        $("header > h1").css("opacity", "0");
-    } else {
-        $("header > h1").css("opacity", "1");
-    }
-})

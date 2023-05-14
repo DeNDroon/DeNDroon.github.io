f8c3e1f5b2d0ecd8c3f925ea41aec22b9ddd0384
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-14 21:26:03 +0400

    menu btn animation

--- frontendmentor/news-page/script.js
+++ frontendmentor/news-page/script.js
@@ -1,7 +1,8 @@
 var menu_btn = document.getElementById("menu-btn");
 var menu_list = document.getElementById("menu-list");
-
+var opened = false;
 menu_btn.addEventListener("click", () => {
+    opened = !opened;
     menu_list.classList.toggle("open-menu");
-    
+    menu_btn.classList.toggle("open-menu")
 })

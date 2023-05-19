7b587ea1b81ad5e067b39cb2a8faddcd027772ae
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-19 23:54:39 +0400

    Improvements in design

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.documentElement.scrollTop = 1080;
+document.addEventListener("scroll", function() {
+    if(document.documentElement.scrollTop > window.innerHeight) {
+        $("header").css("background-color", "black")
+    } else {
+        $("header").css("background-color", "transparent")
+    }
+})

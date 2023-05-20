c09b10aa02105626f08fc8111765aab7cbfc9476
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-20 13:46:13 +0400

    Added skills section with simple temporary animation

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 document.addEventListener("scroll", function() {
     if(document.documentElement.scrollTop > window.innerHeight) {
-        $("header").css("background-color", "black")
+        $("header").css("background-color", "rgb(0,0,0,0.5)")
     } else {
+        // $("#welcome-sec").css("background-position", "center " + -1500 * document.documentElement.scrollTop / window.innerHeight + "px");
         $("header").css("background-color", "transparent")
+        let moveTop = document.documentElement.scrollTop;
+        $("#welcome-sec h1, #welcome-sec pre").css("margin-top", -moveTop).css("opacity", (200 - moveTop) / 200).css("scale", (window.innerHeight - moveTop) / window.innerHeight);
     }
+})
+document.documentElement.scrollTop = 3080;
+document.body.addEventListener("mousemove", function(e) {
+    elem = document.querySelectorAll(".skill");
+    document.querySelectorAll(".skill").forEach(elem => {
+        let posX = (e.pageX - elem.offsetLeft - elem.clientWidth / 2);
+        let posY = (e.pageY - elem.offsetTop - elem.clientHeight / 2);
+        if(posX < 100 && posX > -100) {
+            elem.style.setProperty("--x", posX + "px");
+        }
+        if(posY < 100 && posY > -100) {
+            elem.style.setProperty("--y", posY + "px");
+        }
+    })
 })

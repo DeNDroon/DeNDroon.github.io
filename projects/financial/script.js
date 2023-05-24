060ce2ee8e5ee1c312a518faab13fb5062bdcef4
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-24 15:32:09 +0400

    New Project Upload
    Financial project
    Design from figma community (Autho IDK -_-)

---
+++ projects/financial/script.js
@@ -0,0 +1,27 @@
+$("input[type='radio']").on("change", () => {
+    let id = $("input[type='radio']:checked").attr("id") - 1;
+    let each = window.innerWidth / (window.innerWidth / $(".comment").innerWidth());
+    let margin = "calc(" + -id * each + "px + 5%)";
+    $(".com-carousel").css("margin-left", margin);
+    $(".comment").css("opacity", "0.2");
+    $(`.comment:nth-of-type(${id + 1})`).css("opacity", "1");
+})
+
+let playing = false;
+$(".video-cont > button").on("click", function() {
+    if (!playing) {
+        document.getElementById("video-player").play();
+        $(".video-cont > button").fadeToggle();
+        playing = !playing
+    }
+})
+
+$("#video-player").on("click", () => {
+    if (!playing) {
+        document.getElementById("video-player").play();
+    } else {
+        document.getElementById("video-player").pause();
+    }
+    $(".video-cont > button").fadeToggle();
+    playing = !playing
+})

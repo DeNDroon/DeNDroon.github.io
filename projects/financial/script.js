2ab1652d4b0ed98dbf1c92b98b3bcaa9837f4378
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-25 16:54:38 +0400

    added new features

--- projects/financial/script.js
+++ projects/financial/script.js
@@ -1,10 +1,19 @@
 $("input[type='radio']").on("change", () => {
     let id = $("input[type='radio']:checked").attr("id") - 1;
     let each = window.innerWidth / (window.innerWidth / $(".comment").innerWidth());
-    let margin = "calc(" + -id * each + "px + 5%)";
-    $(".com-carousel").css("margin-left", margin);
+    let scrollLeft = id * each;
+    $(".com-carousel").scrollLeft(scrollLeft);
     $(".comment").css("opacity", "0.2");
     $(`.comment:nth-of-type(${id + 1})`).css("opacity", "1");
+})
+
+$(".com-carousel").on("scroll", () => {
+    let cur = $(".com-carousel").scrollLeft() + 50;
+    let each = window.innerWidth / (window.innerWidth / $(".comment").innerWidth());
+    let id = Math.floor(cur / each) + 1;
+    $(".comment").css("opacity", "0.2");
+    $(`.comment:nth-of-type(${id})`).css("opacity", "1");
+    $("#" + id).click();
 })
 
 let playing = false;

293a1a76dc1ae0664bda72fc4aea3650d8c36f6e
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-28 16:06:32 +0400

    improved visuals

--- projects/financial/menu.js
+++ projects/financial/menu.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
-    // Fetch the header content using AJAX
     $.get('./header.html', function(data) {
-        // Prepend the header content to the body
         $('body').prepend(data);
     });
     $.get('./footer-top.html', function(data) {
-        // Prepend the header content to the body
-      $("footer").prepend(data);
+        $("footer").append(data);
     });
     $.get('./footer-bot.html', function(data) {
-      // Prepend the header content to the body
-      $("footer").prepend(data);
+        $(".foot-top").after(data);
     });
 });

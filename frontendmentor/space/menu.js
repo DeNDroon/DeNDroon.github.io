2d049b6c02a759aa8704ac631db2acd03f73b2c9
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-01 17:50:59 +0400

    Retrying to upload project

--- frontendmentor/space/menu.js
+++
@@ -1,7 +0,0 @@
-$("#menu-btn").on("change", function() {
-    if ($("#menu-btn").prop("checked")) {
-        $("nav").css("transform", "none")
-    } else {
-          $("nav").css("transform", "translate(100%)")
-        }
-    })

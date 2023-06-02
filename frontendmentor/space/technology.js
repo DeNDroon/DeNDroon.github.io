fc8aa1581629956eadb1ff4e006d68b65b35dba6
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-02 13:45:36 +0400

    Almost finshed with animations. Added waiting funcs for img

--- frontendmentor/space/technology.js
+++ frontendmentor/space/technology.js
@@ -20,32 +20,45 @@ $("input[name='space_launch']").on("change", function () {
     }
 })
 
-var curImg = 1;
 var smallSize = "./assets/technology/image-launch-vehicle-portrait.jpg"
 var bigSize = "./assets/technology/image-launch-vehicle-landscape.jpg"
 function changeOver(infoId) {
     $.ajax({
         url: "./data.json",
         dataType: 'json',
-        success: function(jsonData) {
-            var i = jsonData.technology[infoId - 1];
-            $("#technology_name").text(i.name);
-            $("#technology_title").text(i.description);
-            smallSize = i.images.portrait;
-            bigSize = i.images.landscape;
-            placePhoto(bigSize, smallSize);
-            curImg = infoId;
+        success: function (jsonData) {
+            $("#technology_name, #technology_title").css("transform", "rotateX(90deg)")
+
+            setTimeout(function () {
+                var i = jsonData.technology[infoId - 1];
+                $("#technology_name").text(i.name);
+                $("#technology_title").text(i.description);
+                smallSize = i.images.portrait;
+                bigSize = i.images.landscape;
+                placePhoto(bigSize, smallSize);
+                loadImage($("#technology_image").attr("src"), function () {
+                    setTimeout(function() {
+                        $("#technology_name, #technology_title").css("transform", "none")
+                    }, 200)
+                });
+            }, 200)
         }
-      });
+    });
 }
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     placePhoto(bigSize, smallSize)
 }, true);
 
 placePhoto(bigSize, smallSize);
 
-function placePhoto(bigger,smaller) {
+function placePhoto(bigger, smaller) {
     let img = window.innerWidth > 990 ? smaller : bigger;
     $("#technology_image").attr("src", img);
+}
+
+function loadImage(url, callback) {
+    var img = new Image();
+    img.onload = callback;
+    img.src = url;
 }
